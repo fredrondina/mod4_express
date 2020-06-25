751a24cde46dec92e3301d24bd49c9dc4cb6cde0
Fred Rondina <fredrondina96@gmail.com>
2020-06-25 11:08:27 -0600

    Initial Commit

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/publications',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+};
+
+// module.exports = {
+//
+//   development: {
+//     client: 'pg',
+//     connection: {
+//       filename: 'postgres://localhost/publications'
+//     }
+//   },
+
+
+  //
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
